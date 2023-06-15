e978cb807b378883acdc6c52e7e6bbad42f60421
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-06-15 18:21:25 +0300

    feat: bump solc to 0.8.19

--- test/SignatureTransfer.t.sol
+++ test/SignatureTransfer.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import {SafeERC20, IERC20, IERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
